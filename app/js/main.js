9f076547c1cfb9e8eef66e9883533e3933ae0291
Eduard Cot <ecot@itec.cat>
2016-11-07 15:27:36 +0100

    prova test

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
-const name = 'Scotch.io';
+require('../css/main.css');
+require('./logger');
+
+const name = 'ed.cot';
 
 setTimeout(() => alert(`Hello there from ${name}`), 300);
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-    entry: './app/js/main.js',
+    entry: ['./app/js/main.js', './app/js/secondary.js'],
     output: {
         filename: './app/js/bundle.js'
     },
-    watch: true,
 
     module: {
+        preLoaders: [
+            {
+              test: /\.js$/,
+              exclude: /node_modules/,
+              loader: 'jshint-loader'
+
+            }
+        ],
         loaders: [
             {
                 test: /\.js$/,
@@ -14,6 +21,11 @@ module.exports = {
                 query: {
                     presets: ['es2015']
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loader: 'style!css'
             }
         ],
     }
